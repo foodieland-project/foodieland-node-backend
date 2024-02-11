7fa16e3039e4c028056e9b353b36dd4b292bfe5c
alireza babaie <babaie774@gmail.com>
2024-02-11 16:57:00 +0330

    fix: bug

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 router.use('/api', require('./api'));
 
+
 router.get('/', function (req, res, next) {
     res.render('index', { title: 'NodeJS Microservices' });
 });
